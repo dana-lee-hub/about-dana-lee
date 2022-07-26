949415320e2f41c030388edf91b85ac79e318b2d
dana-lee-hub <78123816+dana-lee-hub@users.noreply.github.com>
2022-07-26 16:52:37 -0500

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hello, I'm Dana and I'm going through a Gatsby tutorial.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
